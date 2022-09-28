50e243e18d7b381f6b973688e7683d5859225372
Matthieu Vachon <matt@streamingfast.io>
2022-09-28 16:07:54 -0400

    Updated to latest Aptos block model

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -1,6 +1,7 @@
 pub mod aptos {
     pub mod extractor {
         // @@protoc_insertion_point(attribute:aptos.extractor.v1)
+        #[allow(dead_code)]
         pub mod v1 {
             include!("aptos.extractor.v1.rs");
             // @@protoc_insertion_point(aptos.extractor.v1)
@@ -8,6 +9,7 @@ pub mod aptos {
     }
     pub mod util {
         // @@protoc_insertion_point(attribute:aptos.util.timestamp)
+        #[allow(dead_code)]
         pub mod timestamp {
             include!("aptos.util.timestamp.rs");
             // @@protoc_insertion_point(aptos.util.timestamp)
